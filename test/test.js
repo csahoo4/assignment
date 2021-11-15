acca12ebc3ee862bbc1ccc0f72b0010c54d683fd
Chiranjeeb Sahoo <chiranjeebsahoo4@gmail.com>
2021-11-15 14:29:45 +0530

    update test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,60 @@
 const test = require("../app/app.js")
 const assert = require('assert');
+// const arr = [filterVenuesByCity, filterVenuesBySports, filterVenuesByBooking,
+//     filterVenuesByRating, filterVenuesByAmenities, searchVenuesByName, searchVenuesByCity,
+//     searchVenuesByArea]
 
 function test_filterVenuesByCity() {
     assert(test.filterVenuesByCity("Bangalore")[0].length === 5)
 }
 
-function test_filterVenuesByCity() {
-    assert(test.filterVenuesByCity("Bangalore")[0].length === 5)
+function test2_filterVenuesByCity() {
+    assert(typeof test.filterVenuesByCity("Bangalore") === "object")
 }
 
-test1()
+function test_filterVenuesBySports() {
+    var sports = ""
+    assert(test.filterVenuesBySports(sports) === "Please provide a valid sport name.")
+}
+
+function test2_filterVenuesBySports() {
+    var sports = ["SP1", "SP2"]
+    assert(typeof test.filterVenuesBySports(sports) === "object")
+}
+
+function test_filterVenuesByBooking() {
+    var category = 4
+    assert(test.filterVenuesByBooking(category).length === 0)
+}
+
+function test2_filterVenuesByBooking() {
+    var category = 2
+    assert(test.filterVenuesByBooking(category).length > 0)
+}
+
+function test_filterVenuesByRating() {
+    var avgRating = 5
+    assert(test.filterVenuesByRating(avgRating).length > 0)
+}
+
+function test2_filterVenuesByRating() {
+    var avgRating = null
+    assert(test.filterVenuesByRating(avgRating) === "Please provide a valid rating.")
+}
+
+function test_filterVenuesByAmenities() {
+    var amenities = ""
+    assert(test.filterVenuesByAmenities(amenities) === "Please provide a valid amenity name.")
+}
+
+
+
+test_filterVenuesByCity()
+test2_filterVenuesByCity()
+test_filterVenuesBySports()
+test2_filterVenuesBySports()
+test_filterVenuesByBooking()
+test2_filterVenuesByBooking()
+test_filterVenuesByRating()
+test2_filterVenuesByRating()
+test_filterVenuesByAmenities()
